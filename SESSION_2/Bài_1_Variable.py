efe8cf270e4dba8352236904c35ee7ff1fba513d
hieuminhle <hieuminhle2k@gmail.com>
2019-04-21 21:57:36 +0700

    nhap ban kinh

---
+++ SESSION_2/Bài_1_Variable.py
@@ -0,0 +1,8 @@
+# h = int(input("nhap chieu cao: "))
+# a = int(input('nhap canh day: '))
+# S = h*a/2
+# print('dien tich', S)
+
+r = int(input('nhap ban kinh'))
+S = 3.14 * r**2
+print('dien tich:', S)
